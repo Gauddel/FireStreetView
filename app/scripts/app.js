b4eb1124c757648f1c7ef25335e3f9445554d9ec
Jujudev <abdou.dione@hotmail.com>
2016-10-05 00:12:38 +0200

    Add files via upload

---
+++ app/scripts/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jujuvu2App
+ * @description
+ * # jujuvu2App
+ *
+ * Main module of the application.
+ */
+
+angular.module('jujuvu2App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth',
+    'ngMaterial',
+    'ngAria'
+  ]);
